4752cec3a47bcdec160b5693846e5ddd25c2e9ef
Giuliano Trindade <giuliano.trindade@concrete.com.br>
2016-12-01 17:16:53 -0200

    Treinamento interativo inicio

---
+++ treinamentoInterativo.rb
@@ -0,0 +1,15 @@
+#treinamento interativo if
+a = 23
+b = 34
+
+if a < b
+    print "a menor que b"
+elsif b < a
+    print "b menor que a"
+else
+    print "b igual a a"
+end
+
+problem = false
+
+print "Boa" unless problem    
