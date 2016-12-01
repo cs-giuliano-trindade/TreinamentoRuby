2956176b7d643377d71ae4c5f123290948bf4a3f
Giuliano Trindade <giuliano.trindade@concrete.com.br>
2016-12-01 16:52:36 -0200

    Treinamento Boolean

---
+++ scriptBool.rb
@@ -0,0 +1,39 @@
+is_true = 2 != 3
+
+is_false = 2 == 3
+
+test_1 = 17 > 16
+
+test_2 = 21 < 30
+
+test_3 = 9 <= 9
+
+test_4 = -11 < 4
+
+# boolean_1 = 77 < 78 && 77 < 77
+boolean_1 = false
+
+# boolean_2 = true && 100 >= 100
+boolean_2 = true
+
+# boolean_3 = 2**3 == 8 && 3**2 == 9
+boolean_3 = true
+
+
+# boolean_1 = 2**3 != 3**2 || true
+boolean_1 = true
+
+# boolean_2 = false || -10 > -9
+boolean_2 = false
+
+# boolean_3 = false || false
+boolean_3 = false
+
+# boolean_1 = !true
+boolean_1 = false
+
+# boolean_2 = !true && !true
+boolean_2 = false
+
+# boolean_3 = !(700 / 10 == 70)
+boolean_3 = false
